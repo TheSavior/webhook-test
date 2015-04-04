7298197ecec2bac2ef4b65d41f0b528a5726950e
Eli White <github@eli-white.com>
2015-04-04 14:57:47 -0700

    Using a project-id

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ var Config = {
   browserVersion: process.env.BROWSER_VERSION,
   browserstackUser: process.env.BROWSERSTACK_USER,
   browserstackKey: process.env.BROWSERSTACK_KEY,
+  projectId: '092f9894-26b0-4482-9eba-c287ab99fc62',
   screenshotRoot: path.join(__dirname, 'screenshots', browser),
   api: 'https://visualdiff.ngrok.com/api/',
   websiteUrl: 'http://0.0.0.0:8001'
